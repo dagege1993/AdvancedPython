4d94d901285b0c6c9b9c8562e6083b35bce9944e
hlz <18273711329@163.com>
2018-10-10 22:02:10 +0800

    python中一切皆对象

--- chapter02/company.py
+++ chapter02/company.py
@@ -1,14 +1,28 @@
+# class Company(object):
+#     def __init__(self, employee_list):
+#         self.employee = employee_list
+#
+#     # 添加魔法函数,类实例化的对象是可迭代的
+#     def __getitem__(self, item):
+#         return self.employee[item]
+#
+#
+# company = Company(['tom', 'bob', 'jane'])
+# print(company)
+# emploee = company.employee
+# print(emploee)
+# for em in company:
+#     print(em)
+
+
 class Company(object):
     def __init__(self, employee_list):
         self.employee = employee_list
 
     # 添加魔法函数,类实例化的对象是可迭代的
-    def __getitem__(self, item):
-        return self.employee[item]
+    def __str__(self):
+        return ','.join(self.employee)
 
 
 company = Company(['tom', 'bob', 'jane'])
-emploee = company.employee
-print(emploee)
-for em in company:
-    print(em)
+print(company)
